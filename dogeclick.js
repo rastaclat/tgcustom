b558b6f1f072de4f33add517820001ad1c3831e8
Mable Morar <52530606+rastaclat@users.noreply.github.com>
2024-09-23 22:11:21 +0800

    Update dogeclick.js

--- dogeclick.js
+++ dogeclick.js
@@ -5,8 +5,8 @@
 // @description  模拟鼠标点击
 // @author       You
 // @match        https://*.babydogeclikerbot.com/*
-// @updateURL    https://raw.githubusercontent.com/rastaclat/tgcustom/main/dogeclick.js
-// @downloadURL  https://raw.githubusercontent.com/rastaclat/tgcustom/main/dogeclick.js
+// @updateURL    https://github.com/rastaclat/tgcustom/blob/main/dogeclick.js
+// @downloadURL  https://github.com/rastaclat/tgcustom/blob/main/dogeclick.js
 
 // @grant        none
 // ==/UserScript==
